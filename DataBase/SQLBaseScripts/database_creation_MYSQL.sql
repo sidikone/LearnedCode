5da439bb673588de7f2f659ed7dc82c57c9f6916
sidikone <aboubakone@gmail.com>
2024-09-05 15:20:59 +0200

    delete elements

--- DataBase/SQLBaseScripts/database_creation_MYSQL.sql
+++
@@ -1,8 +0,0 @@
--- Create a new database named 'demo'
-CREATE DATABASE demo;
-
--- Switch to the 'demo' database
-USE demo;
-
--- Show all tables in the currently selected database (which is 'demo')
-SHOW TABLES;
--- DataBase/SQLBaseScripts/queries_tab.sql
+++
@@ -1,4 +0,0 @@
-USE demo;  -- Switch to the 'demo' database
-
--- Show detailed information for each column in the 'comment_telephone' table
-SHOW FULL COLUMNS FROM comment_telephone;  
