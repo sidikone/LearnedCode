b4cc3cbbdf485113b82e858425dadd3cab886cc2
sidikone <aboubakone@gmail.com>
2024-09-08 01:50:57 +0200

    table over

---
+++ DataBase/SQLBaseScripts/DDL/Constraints/create_enrollments_table_pk_fk.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Enrollments (
+    enrollment_id INT,                  -- Primary key for the enrollment
+    student_id INT,                     -- Foreign key referencing the student
+    course_id INT,                      -- Foreign key referencing the course
+
+    CONSTRAINT pk_enrollment_id         -- Named primary key constraint
+    PRIMARY KEY (enrollment_id),
+
+    CONSTRAINT fk_student               -- Named foreign key constraint for the student table
+    FOREIGN KEY (student_id)           
+    REFERENCES students(student_id),
+
+    CONSTRAINT fk_course               -- Named foreign key constraint for the course table
+    FOREIGN KEY (course_id)
+    REFERENCES course(student_id)
+
+);
+/*
+1. Primary Key  : Named pk_enrollment_id
+2. Foreign Keys : The constraints for foreign keys are named fk_student and fk_course
+*/
